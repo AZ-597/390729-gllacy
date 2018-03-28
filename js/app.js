d86b66f6e99f088809b4cd2a79a4e8139cd7eaeb
Andy Z <az-597@yandex.ru>
2018-03-28 20:52:53 +0300

    Пофиксил косяки #1.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 var modal = document.querySelector(".modal");
-var modalForm = document.querySelector(".get-in-touch");
 var openModal = document.querySelector(".js-open-modal");
 var closeModal = document.querySelector(".js-close-modal");
 
 openModal.addEventListener('click', function(evt) {
   evt.preventDefault();
   modal.classList.add("modal-in-animation");
-  modalForm.classList.add("modal-form-in-animation");
   modal.style.display = "flex";
   setTimeout(function() {
     modal.classList.remove("modal-in-animation");
-    modalForm.classList.remove("modal-form-in-animation");
   }, 600);
 });
 
@@ -25,11 +22,3 @@ closeModal.addEventListener('click', function(evt) {
   }, 490);
 
 });
-/*
-modal.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  modal.style.display = "none";
-});
- */
-// надо бы наверное почитать про всплытие событий, делигирование или что-то такое,
-// потому что при клике на детей модалка тоже закрывается :(
